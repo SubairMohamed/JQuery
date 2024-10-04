871c69f1b819b4f2c5151179426d9e5ecf0b3bb5
SubairMohamed <SubairMohamed44@gmail.com>
2024-10-04 22:49:51 +0100

    well

--- Event.js
+++ Event.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
 
     // Mouse Event Method
     $("p").click(function () {
-       $(this).hide();
+       //$(this).hide();
+       //$("p").hide();
     });
 
 });
