6804aca53af83e539d8b65989c4038840ec621b2
SubairMohamed <SubairMohamed44@gmail.com>
2024-12-04 22:37:36 +0100

    stil

--- Selectors.js
+++ Selectors.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
         $("li:not(.myclass)").css("background-color", "yellow");
         $("ul li:first").css("color", "red");
         $("ul li:last").css("color", "red");
+        $("div p").css("color", "gray");
         $("p.para").css("color", "blue");
 });
