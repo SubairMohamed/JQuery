fdee069d64b1c661abcd2a2f84f407e5fe5273db
SubairMohamed <SubairMohamed44@gmail.com>
2024-12-04 22:21:53 +0100

    first

--- Selectors.js
+++ Selectors.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
         $("p >").css("color", "blue");
         $("li:not(.myclass)").css("background-color", "yellow");
+        $("ul li:first").css("color", "red");
 });
