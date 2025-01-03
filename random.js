16c6ab0ad497f14c640a4debecad37c851e68fea
SubairMohamed <SubairMohamed44@gmail.com>
2025-01-03 22:15:37 +0100

    well

---
+++ random.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        // let generator = Math.ceil(Math.random () * 10);
+        // let generator = Math.random () * 10;
+        $("#numberDisplay").text(generator);
+    });
+});
